af03db0115bd58743ca9aecdc05b2ce500732700
zestdoc <zestdoc@gmail.com>
2021-12-09 07:38:03 -0500

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,6 +1,8 @@
 from model_training_service import Code
 #from const import API_KEY
 import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
 
 def app():
@@ -19,7 +21,7 @@ def app():
         # Setting up the Title
           
     st.title("Echo explain")
-    #st.header("Powered by Cordoc LLC") 
+    st.header("Powered by Cordoc LLC") 
 
     st.write(f"""
         ## Made by Cordoc LLC
@@ -47,3 +49,26 @@ def app():
    # else:
     #    st.error("🔑 API Key Not Found!")
    #     st.info("💡 Copy paste your OpenAI API key that you can find in User -> API Keys section once you log in to the OpenAI API Playground")
+
+   # Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets.gcp_service_account,
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets.private_gsheets_url
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
