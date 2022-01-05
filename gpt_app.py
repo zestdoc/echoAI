ee550601d320b7e3d59ca72ba3283bf20dd0545d
zestdoc <zestdoc@gmail.com>
2022-01-05 15:08:57 -0500

    Update gpt_app.py

--- gpt_app.py
+++ gpt_app.py
@@ -6,8 +6,13 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Echo Explain",
-    page_icon="�:heart:",
-    layout="centered")
+    page_icon=":heart:",
+    layout="centered",
+    menu_items={
+         'Get Help': 'https://www.cordoc.com/help',
+         'Report a bug': "https://www.cordoc.com/bug",
+         'About': "# Cordoc LLC.  THIS WEBSITE DOES NOT PROVIDE MEDICAL ADVICE. It is intended for informational purposes only. It is not a substitute for professional medical advice, diagnosis or treatment. Never ignore professional medical advice in seeking treatment because of something you have read on this  website. If you think you may have a medical emergency, immediately call your doctor or dial 911. "
+     })
 
 # Pages as key-value pairs
 PAGES = {
