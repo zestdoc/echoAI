2ef96dbd156c94fff0c1d8ff90d0e1706864185e
zestdoc <zestdoc@gmail.com>
2022-01-05 15:40:32 -0500

    Update training_data.py

--- training_data.py
+++ training_data.py
@@ -7,11 +7,6 @@ Output: Normal pumping function of the left and right side of the heart. Heart v
 
 -
 
-Input: severely dilated left ventricle with severely reduced systolic function. EF 25 %. Mildly dilated right ventricle with moderately reduced systolic function. Dilated mitral annulus with moderate central mitral regurgitation. Dilated ascending aorta with moderate central aortic regurgitation. Prior report was reviewed and systolic function as well as mitral regurgitation appears worse
-
-Output: Heart is enlarged with reduced pumping if the right as well as left side of the heart. Mitral valve is severely leaky. Large artery coming out of the heart (aorta) is enlarged. Aortic valve is moderately leaky. These findings are worse compared to an earlier study
-
--
 Input: {}
 
 Output:"""
